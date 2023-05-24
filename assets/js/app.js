44ffaca318f9751600137694838d9b9b58b4cd53
HannanBhuiyan <hannan.diuh@gmail.com>
2023-05-24 12:34:11 +0600

    upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,6 +60,10 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     effect: "fade",
     loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
